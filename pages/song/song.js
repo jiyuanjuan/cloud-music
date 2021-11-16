c9a06cf35772292b0be3e3fc1d922837121763ef
myzjy <365246865@qq.com>
2021-11-16 17:28:12 +0800

    song updata

--- pages/song/song.js
+++ pages/song/song.js
@@ -1,66 +1,26 @@
-// pages/song/song.js
+import request from "../../service/request"
+
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        isPlay: false,
+        songName:'',
+        songPic:''
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    onLoad: async function (options) {
+        let ids = options ? options.musicId:''
+        let result = await request('/song/detail', {
+            ids
+        })
+        this.setData({
+            songName:result.songs[0].name,
+            songPic:result.songs[0].al.picUrl
+        })
+        console.log(result.songs[0])
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    play() {
+        this.setData({
+            isPlay: !this.data.isPlay
+        })
     }
 })
--- pages/songDay/songDay.js
+++ pages/songDay/songDay.js
@@ -9,7 +9,7 @@ Page({
 
     onLoad: async function () {
         let result = await request('/recommend/songs')
-        console.log(result.recommend.slice(0, 20))
+        // console.log(result.recommend.slice(0, 20))
         this.setData({
             day: new Date().getDate(),
             month: new Date().getMonth() + 1,
@@ -17,9 +17,9 @@ Page({
         })
     },
     toSongDetail(event) {
-        console.log(event.currentTarget.dataset.ids)
+        // console.log(event.currentTarget.dataset.ids)
         wx.navigateTo({
-          url: '/pages/song/song',
+          url: `/pages/song/song?musicId=${event.currentTarget.dataset.ids}`,
         })
     }
 })
