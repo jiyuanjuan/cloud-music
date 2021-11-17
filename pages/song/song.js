eca5ed0c970fef18ecb7728e036e13aae9655e0d
myzjy <365246865@qq.com>
2021-11-17 15:38:06 +0800

    song updata

--- pages/song/song.js
+++ pages/song/song.js
@@ -1,26 +1,93 @@
 import request from "../../service/request"
 
+let playControl = wx.createInnerAudioContext()
+let appSong = getApp()
 Page({
     data: {
         isPlay: false,
-        songName:'',
-        songPic:''
+        songName: '',
+        songPic: '',
+        songId: '',
+        songIdx: ''
     },
 
     onLoad: async function (options) {
-        let ids = options ? options.musicId:''
+        let ids = options ? options.musicId : ''
+        let idx = options ? options.musicIdx : ''
         let result = await request('/song/detail', {
             ids
         })
-        this.setData({
-            songName:result.songs[0].name,
-            songPic:result.songs[0].al.picUrl
+        let songUrl = await request('/song/url', {
+            id: ids
         })
-        console.log(result.songs[0])
+        this.setData({
+            songId: ids,
+            songIdx: idx,
+            songName: result.songs[0].name,
+            songPic: result.songs[0].al.picUrl
+        })
+        playControl.src = songUrl.data[0].url
     },
     play() {
         this.setData({
             isPlay: !this.data.isPlay
         })
-    }
+        this.data.isPlay ? playControl.play() : playControl.pause()
+    },
+
+    // 上一曲和下一曲
+    async switchSong(e) {
+        let type = e.currentTarget.id
+        if (type === 'pre' && this.data.songIdx > 0) {
+            let idx = this.data.songIdx - 1
+            let preId = appSong.globalData.songDayList[idx]
+            let result = await request('/song/detail', {
+                ids: preId
+            })
+            let songUrl = await request('/song/url', {
+                id: preId
+            })
+            if (idx === 0) {
+                wx.showToast({
+                    title: '已经是第一首歌了',
+                })
+            }
+            this.setData({
+                isPlay: false,
+                songId: preId,
+                songIdx: idx,
+                songName: result.songs[0].name,
+                songPic: result.songs[0].al.picUrl
+            })
+            playControl.src = songUrl.data[0].url
+        } else {
+            let type = e.currentTarget.id
+            if (type === 'next' && this.data.songIdx < 20) {
+                let idx = this.data.songIdx - 0 + 1
+                let nextId = appSong.globalData.songDayList[idx]
+                let result = await request('/song/detail', {
+                    ids: nextId
+                })
+                let songUrl = await request('/song/url', {
+                    id: nextId
+                })
+                if (idx === 19) {
+                    wx.showToast({
+                        title: '已经是最后一首歌了',
+                    })
+                }
+                this.setData({
+                    isPlay: false,
+                    songId: nextId,
+                    songIdx: idx,
+                    songName: result.songs[0].name,
+                    songPic: result.songs[0].al.picUrl
+                })
+                playControl.src = songUrl.data[0].url
+            } 
+        }
+    },
+    onUnload: function () {
+        playControl.stop()
+    },
 })
--- pages/songDay/songDay.js
+++ pages/songDay/songDay.js
@@ -1,5 +1,5 @@
 import request from "../../service/request"
-
+const app = getApp()
 Page({
     data: {
         day: '',
@@ -7,19 +7,19 @@ Page({
         list: []
     },
 
-    onLoad: async function () {
+    onLoad: async function () {  
+        app.globalData.songDayList = []
         let result = await request('/recommend/songs')
-        // console.log(result.recommend.slice(0, 20))
+        result.recommend.slice(0, 20).map(item=>{app.globalData.songDayList.push(item.id)})
         this.setData({
-            day: new Date().getDate(),
+            day: new Date().getDate(), 
             month: new Date().getMonth() + 1,
             list: result.recommend.slice(0, 20)
         })
     },
     toSongDetail(event) {
-        // console.log(event.currentTarget.dataset.ids)
         wx.navigateTo({
-          url: `/pages/song/song?musicId=${event.currentTarget.dataset.ids}`,
+          url: `/pages/song/song?musicId=${event.currentTarget.dataset.ids}&musicIdx=${event.currentTarget.dataset.idxsong}`,
         })
     }
 })
