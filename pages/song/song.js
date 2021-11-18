6f65418f61ee5fe095926d64148038189fa9d581
myzjy <365246865@qq.com>
2021-11-18 16:51:05 +0800

    playTime

--- pages/song/song.js
+++ pages/song/song.js
@@ -1,14 +1,18 @@
 import request from "../../service/request"
+import momemt from "moment"
 
 let playControl = wx.createInnerAudioContext()
 let appSong = getApp()
+
 Page({
     data: {
         isPlay: false,
         songName: '',
         songPic: '',
         songId: '',
-        songIdx: ''
+        songIdx: '',
+        playTime: '',
+        durationTime: ''
     },
 
     onLoad: async function (options) {
@@ -20,13 +24,25 @@ Page({
         let songUrl = await request('/song/url', {
             id: ids
         })
+        playControl.src = songUrl.data[0].url
         this.setData({
             songId: ids,
             songIdx: idx,
             songName: result.songs[0].name,
-            songPic: result.songs[0].al.picUrl
+            songPic: result.songs[0].al.picUrl,
         })
-        playControl.src = songUrl.data[0].url
+        playControl.onCanplay(() => {
+            this.setData({
+                playTime: momemt(playControl.currentTime * 1000).format('mm:ss'),
+                durationTime: momemt(playControl.duration * 1000).format('mm:ss')
+            })
+        })
+        playControl.onTimeUpdate(() => {
+            this.setData({
+                playTime: momemt(playControl.currentTime * 1000).format('mm:ss'),
+                durationTime: momemt(playControl.duration * 1000).format('mm:ss')
+            })
+        })
     },
     play() {
         this.setData({
@@ -34,57 +50,44 @@ Page({
         })
         this.data.isPlay ? playControl.play() : playControl.pause()
     },
-
+    async switchPreOrNext(num) {
+        this.setData({
+            playTime: momemt(playControl.currentTime * 1000).format('mm:ss'),
+            durationTime: momemt(playControl.duration * 1000).format('mm:ss')
+        })
+        let idx = this.data.songIdx - num
+        let preId = appSong.globalData.songDayList[idx]
+        let result = await request('/song/detail', {
+            ids: preId
+        })
+        let songUrl = await request('/song/url', {
+            id: preId
+        })
+        if (idx === 0) {
+            wx.showToast({
+                title: '已经是第一首歌了',
+            })
+        }
+        this.setData({
+            isPlay: false,
+            songId: preId,
+            songIdx: idx,
+            songName: result.songs[0].name,
+            songPic: result.songs[0].al.picUrl
+        })
+        playControl.src = songUrl.data[0].url
+    },
     // 上一曲和下一曲
     async switchSong(e) {
+        playControl.stop()
         let type = e.currentTarget.id
         if (type === 'pre' && this.data.songIdx > 0) {
-            let idx = this.data.songIdx - 1
-            let preId = appSong.globalData.songDayList[idx]
-            let result = await request('/song/detail', {
-                ids: preId
-            })
-            let songUrl = await request('/song/url', {
-                id: preId
-            })
-            if (idx === 0) {
-                wx.showToast({
-                    title: '已经是第一首歌了',
-                })
-            }
-            this.setData({
-                isPlay: false,
-                songId: preId,
-                songIdx: idx,
-                songName: result.songs[0].name,
-                songPic: result.songs[0].al.picUrl
-            })
-            playControl.src = songUrl.data[0].url
+            this.switchPreOrNext(1)
         } else {
             let type = e.currentTarget.id
             if (type === 'next' && this.data.songIdx < 20) {
-                let idx = this.data.songIdx - 0 + 1
-                let nextId = appSong.globalData.songDayList[idx]
-                let result = await request('/song/detail', {
-                    ids: nextId
-                })
-                let songUrl = await request('/song/url', {
-                    id: nextId
-                })
-                if (idx === 19) {
-                    wx.showToast({
-                        title: '已经是最后一首歌了',
-                    })
-                }
-                this.setData({
-                    isPlay: false,
-                    songId: nextId,
-                    songIdx: idx,
-                    songName: result.songs[0].name,
-                    songPic: result.songs[0].al.picUrl
-                })
-                playControl.src = songUrl.data[0].url
-            } 
+            this.switchPreOrNext(-1)
+            }
         }
     },
     onUnload: function () {
